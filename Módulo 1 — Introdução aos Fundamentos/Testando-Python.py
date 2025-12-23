9b156ad53510a46e85ec82825fd81411c1366761
João Henrique C. B. Miranda <johnbernardprograming@gmail.com>
2025-12-23 09:46:27 -0300

    modified:   "M\303\263dulo 1 \342\200\224 Introdu\303\247\303\243o aos Fundamentos/[ 0 ] Primeiro Contato (Output).ipynb"

---
+++ Módulo 1 — Introdução aos Fundamentos/Testando-Python.py
@@ -0,0 +1,19 @@
+# Mostrando Informações na Tela
+
+print("Olá, Mundo!")
+
+print("Estou Testando como que é usar a função print(), está sendo muito legal!")
+
+print("Python é uma linguagem de programação muito poderosa e versátil.")
+
+print(20)
+
+print(30 + 20)
+
+print("O resultado da soma é:", 30 +20)
+
+
+print(f"Agora irei testar o formato de formatação. O Resultado da soma é {30 +20}")
+
+print("Fim do teste!")
+
